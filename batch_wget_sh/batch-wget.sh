e01bf37fe61a05c0ef58afb0de394cd12787bbf9
yo1995 <cht@bupt.edu.cn>
2021-03-02 11:02:23 -0800

    Add shell scripts.

---
+++ batch_wget_sh/batch-wget.sh
@@ -0,0 +1,4 @@
+for i in {257..510..2}
+do 
+    wget "https://wordpress.org/uploads/allimg/2021/02/23/K$i.webp";
+done
---
+++ execute_every_13hrs_sh/hours_13.sh
@@ -0,0 +1,2 @@
+python3 [your py scripts]
+at -f [full path to this shell script] -v now + 13 hours
